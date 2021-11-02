83fb65c09177834ad7f12a227f0fe107c647adfe
Your Name <you@example.com>
2021-11-02 17:34:17 +0900

    repair complext calculation step

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import taichi as ti
-import numpy as np 
+import numpy as np
+
+from taichi.lang.ops import sqrt 
 ti.init(arch=ti.gpu)
 
 n = 320
@@ -11,14 +13,25 @@ memo_rt = []
 
 @ti.func
 def complex_sqr(z)->ti.Vector:
-	return ti.Vector([3 *z[0]**2 - z[1]**2, 3*z[1] * z[0] * 2])
+	return ti.Vector([z[0]**2 - z[1]**2, z[1] * z[0] * 2])
+
+@ti.func
+def ndegreeFunc(z,n):
+    for _ in range(n-1):
+        z = complex_sqr(z)
+    return z 
+
+@ti.func
+def func(z,n):
+    val = ndegreeFunc(z,n)
+    return ti.Vector([val[0]- 1, val[1]])
 
 @ti.func
 def complex_func(z)->ti.Vector:
 	return ti.Vector([z[0]**3 - 3*z[0]*z[1]**2 -1, -z[1]**3 + 3*z[0] * z[1] ** 2])
 
 @ti.func
-def complex_div(x,y):
+def complex_div(x,y)->ti.Vector:
     return ti.Vector(
        [ 
            (x[0]*y[0] + x[1]*y[1])/(y.norm()**2 + 1e-3), 
@@ -29,39 +42,30 @@ def complex_div(x,y):
 def main(t:float):
     for i,j in pixels:
         iter_num = 0
-        z = ti.Vector([i / n - 1 + ti.cos(t)*0.02 , j / n - 0.5 + ti.sin(t) *0.02]) * 5
+        z = ti.Vector([i / n - 1  , j / n - 0.5]) * 5
+        time_rotate_mat = ti.Vector([
+            [ti.cos(t),ti.sin(t)],
+            [-ti.sin(t),ti.cos(t)]
+            ])
+        
+        z = time_rotate_mat @ z
         threshold = 1000
-        func_val = complex_func(z)
-        grad_val = complex_sqr(z)
+        func_val = func(z,6)
+        grad_val = ndegreeFunc(z,5)
 
-        nxt_pos = z - complex_div(func_val, grad_val)
+        diff = z - complex_div(func_val, grad_val)
 
-        while (z-nxt_pos).norm() > 1e-4 and iter_num < threshold:
-            z = nxt_pos
-            func_val = complex_func(z)
-            if func_val.norm()>1e10:
-                break
-            grad_val = complex_sqr(z)
-            nxt_pos = z - complex_div(func_val, grad_val)
+        while diff.norm() > 1e-4 and iter_num < threshold:
+            z = diff
+            func_val = func(z,6)
+            grad_val = ndegreeFunc(z,5)
 
+            diff = z - complex_div(func_val, grad_val)
             iter_num+=1
-        #solution
-        z= nxt_pos
-        # isExsit =False
-        # ind = 0
-        # if z.all():
-        #     for r in memo_rt:
-        #         if (r-z).norm()<1e-4:
-        #             isExsit = True
-        #             z = r
-        #             break
-        #         ind +=1
-            
-        #     if not isExsit:
-        #         memo_rt.append(z)
-        #         ind= len(memo_rt)
 
-        pixels[i, j] = 1 - z.norm() *0.02
+        z= diff
+        print(z.norm()!= z.norm())
+        pixels[i, j] = 1 - z.norm()/(5*sqrt(2))
 
 gui = ti.GUI("Newton Set", res=(n * 2, n))
 
